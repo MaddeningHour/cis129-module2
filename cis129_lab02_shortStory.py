9b24cec30ee3ece8673b3b015bf778a9e0cc1c34
MaddeningHour <CRoblesSerrano@mail.pima.edu>
2024-09-18 09:36:31 -0700

    Update cis129_lab02_shortStory.py
    Removed the hello world print statement. Updated headers. New print statements added for the first paragraph of "Ender's Game".

--- cis129_lab02_shortStory.py
+++ cis129_lab02_shortStory.py
@@ -1,1 +1,17 @@
-print('Hello, world')
+#Author: Christopher Robles Serrano
+# My first Python lab in my CIS129 course
+# Prints a simple short story
+
+#The first paragraph of Ender's Game by Orson Scott Card
+print("I’ve watched through his eyes, I’ve listened through his ears, and I tell you he’s the one. Or at least as close as we’re going to get.")
+print("That’s what you said about the brother.")
+print("The brother tested out impossible. For other reasons. Nothing to do with his ability.")
+print("Same with the sister. And there are doubts about him. He’s too malleable. Too willing to submerge himself in someone else’s will.")
+print("Not if the other person is his enemy.")
+print("So what do we do? Surround him with enemies all the time?")
+print("If we have to.")
+print("I thought you said you liked this kid.")
+print("If the buggers get him, they’ll make me look like his favorite uncle.")
+print("All right. We’re saving the world, after all. Take him")
+
+
